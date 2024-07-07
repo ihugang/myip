fbca3239aab835edff5498da89e7c1ad7f7dc7c7
Shrek <hugang@gmail.com>
2024-07-07 14:14:51 +0800

    Initial Commit

---
+++ myipTests/myipTests.swift
@@ -0,0 +1,17 @@
+//
+//  myipTests.swift
+//  myipTests
+//
+//  Created by Hu Gang on 2024/7/7.
+//
+
+import Testing
+@testable import myip
+
+struct myipTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
